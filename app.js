e69c706f22a4c0a04baeccd43fef6f23212d3161
Xandria Bramble <xabpinkninja13@gmail.com>
2019-10-24 10:36:08 -0400

    finished css and added js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var age = prompt('How old are you?');
+
+if (age <= 18)
+{
+    alert('Error: you na go no party.');
+}
+    else if (age > 18) 
+    {
+        alert('You gon party ah?');
+    }
+   -
+
+document.write('<h3>' + age + '</h3>')
